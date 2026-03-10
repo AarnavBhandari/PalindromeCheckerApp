2d6237b0be3be2a08a2350ee8fccabd2ce0a1397
AarnavBhandari <aarnav1511@gmail.com>
2026-03-10 15:54:55 +0530

    UC9: Check palindrome using recursion

--- src/main.java
+++ src/main.java
@@ -1,61 +1,20 @@
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class Main {
 
-    // Function to check palindrome
-    public static boolean isPalindrome(Node head) {
-        Node slow = head, fast = head;
-        Node prev = null, temp;
-
-        // Reverse first half while finding middle
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-
-            // Reverse current node
-            temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
+    // Recursive function to check palindrome
+    public static boolean isPalindrome(String word, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-
-        // If odd number of elements, skip middle
-        if (fast != null) {
-            slow = slow.next;
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
         }
-
-        // Compare reversed first half with second half
-        while (prev != null && slow != null) {
-            if (prev.data != slow.data) {
-                return false;
-            }
-            prev = prev.next;
-            slow = slow.next;
-        }
-
-        return true;
+        return isPalindrome(word, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
-        String word = "racecar";
+        String word = "level";
 
-        // Build linked list
-        Node head = new Node(word.charAt(0));
-        Node current = head;
-        for (int i = 1; i < word.length(); i++) {
-            current.next = new Node(word.charAt(i));
-            current = current.next;
-        }
-
-        // Check palindrome
-        if (isPalindrome(head)) {
+        if (isPalindrome(word, 0, word.length() - 1)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
