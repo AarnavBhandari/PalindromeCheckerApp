6138817fd3984207ebf41ee453ebeed417850235
AarnavBhandari <aarnav1511@gmail.com>
2026-03-10 15:43:39 +0530

    UC4: Convert string to char array and compare characters

--- src/main.java
+++ src/main.java
@@ -2,16 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String word = "racecar";
-        String reversed = "";
+        String word = "madam";
 
-        // reverse the string
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        char[] chars = word.toCharArray();
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // check palindrome
-        if (word.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
